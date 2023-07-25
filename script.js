e936bc71614180dec3be6830131548cf837d2e3d
Evelina Bisell <evelina.bisell@gmail.com>
2023-07-25 15:08:46 +0900

    Changed to updated resumes

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
 
     // Show English PDF file in a new tab on English button click
     $("#english-button").on("click", function () {
-        window.open("https://evelinabisell.github.io/Resumes/CV_Resume_Eng_June_2023.pdf");
+        window.open("https://evelinabisell.github.io/Resumes/CV_Eng_June_2023.pdf");
     });
 
     // Show Swedish PDF file in a new tab on Swedish button click
     $("#swedish-button").on("click", function () {
-        window.open("https://evelinabisell.github.io/Resumes/CV_Sve_June_2023.pdf");
+        window.open("https://evelinabisell.github.io/Resumes/CV_Sve_July_2023.pdf");
     });
 
     // Close the popup when user clicks outside the content area
